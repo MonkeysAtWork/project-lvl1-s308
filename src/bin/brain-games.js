c86fc7e03bcb957691fa1e022c99a395c6e63ef8
Freddy <sk3250@mail.ru>
2018-07-09 18:19:20 +0500

    step 2 with logic error

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
+import {userName} from '..'
 
-console.log('Welcome to the Brain Games!');
+console.log('Welcome to the Brain Games!' + '\n' + '');
+console.log(`Hello, ${userName}!`);
+
+
+
+
